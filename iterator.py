39762ca6027b3ee58119c0a59d6aee10dc6044e5
Hossein samiei <hossein@Hosseins-MacBook-Pro.local>
2022-02-27 20:05:37 +0330

    Reverse function working properly , :)

--- iterator.py
+++ iterator.py
@@ -1,21 +1,28 @@
 class Reverse:
     # TO DO
+    x = 0 
     def __init__(self ,list):
         self.list = list
+        
+        
     pass
 
     def __iter__(self):
-        return self.list[len(self.list)]
+        # self.itr = self.list[len(self.list)-1]
+        self.x = len(self.list)
+        return self 
 
     def __next__(self):
-        pass
+        self.x = self.x - 1 
+        if self.x != -1 : 
+            return self.list[self.x]
+        else :
+            raise StopIteration
+        
 
 ls = [10, 20, 30]
 
-it = iter(ls)
-print(type(it))
-print(type(next(it)))
 
-# for it in Reverse(ls):
-#     print(it)
-# print(ls)
+for it in Reverse(ls):
+    print(it)
+print(ls)
